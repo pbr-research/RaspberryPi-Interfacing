16f1a6401d25e439c9fa635696f2957a033b338f
pbr-research <76119933+pbr-research@users.noreply.github.com>
2022-03-22 00:27:48 +0530

    Update ADS1115_simpletest.py

--- ADS1115/ADS1115_simpletest.py
+++ ADS1115/ADS1115_simpletest.py
@@ -5,30 +5,54 @@ This python program will read sample from ADC ADS1115 and display it on terminal
 
 Python version : Python3
 Tested on : Raspberry pi 3B+ and Raspberry pi Zero W
-Author : Bahubali Fuladi
-Date : 19/01/2022 7:59Pm
+Author : 
+Date : 22/03/2022 12:30Am
 """ 
 
 import time
-import board
-import busio
-import adafruit_ads1x15.ads1015 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
 
-# Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
 
-# Create the ADC object using the I2C bus
-ads = ADS.ADS1015(i2c)
 
-# Create single-ended input on channel 0
-chan0 = AnalogIn(ads, ADS.P0)
-chan1 = AnalogIn(ads, ADS.P1)
-# Create differential input between channel 0 and 1
-# chan = AnalogIn(ads, ADS.P0, ADS.P1)
+# Create an ADS1115 ADC (16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115() 
 
-print("{:>5}\t{:>5}".format("raw", "v"))
 
+# Note you can change the I2C address from its default (0x48), and/or the I2C
+# bus by passing in these optional parameters:
+#adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
+
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 1
+
+print('Reading ADS1x15 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+print('-' * 37)
+# Main loop.
 while True:
-	print("{:>5}\t{:>5.3f}   {:>5}\t{:>5.3f}".format(chan0.value, chan0.voltage, chan1.value, chan1.voltage))
-	time.sleep(0.5)
+    # Read all the ADC channel values in a list.
+    values = [0]*4
+    for i in range(4):
+        # Read the specified ADC channel using the previously set gain value.
+        values[i] = adc.read_adc(i, gain=GAIN)
+        # Note you can also pass in an optional data_rate parameter that controls
+        # the ADC conversion time (in samples/second). Each chip has a different
+        # set of allowed data rate values, see datasheet Table 9 config register
+        # DR bit values.
+        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+        # Each value will be a 12 or 16 bit signed integer value depending on the
+        # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+    # Print the ADC values.
+    print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+    # Pause for half a second.
+    time.sleep(0.5)
